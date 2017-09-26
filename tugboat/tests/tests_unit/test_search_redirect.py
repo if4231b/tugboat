4d1287f72a7b485bae669fd897a1471cd83d9c97
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2017-09-26 15:53:41 -0400

    search redirect for several fields
    specifically: author, author_xct, aut_logic, object, obj_logic, title, ttl_logic, text, txt_logic, start_mon, start_year, end_mon, end_year

--- tugboat/tests/tests_unit/test_search_redirect.py
+++ tugboat/tests/tests_unit/test_search_redirect.py
@@ -7,29 +7,133 @@ sys.path.append(PROJECT_HOME)
 
 import unittest
 from requests import Request
-#from flask.ext.testing import TestCase
+import urllib
 from unittest import TestCase
+from datetime import datetime
 
 from tugboat.views import ClassicSearchRedirectView
 
 class TestSearchParameters(TestCase):
     """
-    Test each of the possble classic search parametrs
+    Test each of the possble classic search parameters
+
+    Most test are for authors because the application code uses the same libraries for all the fields
     """
 
-    def test_classic_parameters(self):
+    def test_classic_parameters_authors(self):
         # create prepared request to send to convert url code
-        req = Request('get', 'http://test.test?author=huchra%2C+john')
+        req = Request('get', 'http://test.test?') 
         prepped = req.prepare()
         req.args = {}
         req.mimetype = None
         empty_search = ClassicSearchRedirectView.convert_search(req)
-        self.assertEqual(empty_search, 'q=*:*', 'author test')
+        self.assertEqual('q=*:*', empty_search, 'author test')  # no authors
 
-        req.args = {'author': 'huchra%2C+john'}
-        req.mimetype = None
+        req.args = {'author': urllib.quote('Huchra,+John')}
         author_search = ClassicSearchRedirectView.convert_search(req)
-        self.assertEqual(author_search, 'q=author:"huchra, john"', 'author test')
+        # parentheses are not urlencoded
+        self.assertEqual('q=' + urllib.quote('author:') + '(' + urllib.quote('"Huchra, John"') + ')', 
+                         author_search) # single author no quotes
+
+        req.args = {'author': urllib.quote('"Huchra,+John"')}
+        author_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('author:') + '(' + urllib.quote('"Huchra, John"') + ')', 
+                         author_search) # single author with quotes
+
+        req.args = {'author': urllib.quote('Huchra, John\r\nMacri, Lucas M.')} 
+        author_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('author:') + '(' + urllib.quote('"Huchra, John" AND "Macri, Lucas M."') + ')',
+                         author_search) # authors, newline separator
+
+        req.args = {'author': urllib.quote('Huchra,+John;Macri,+Lucas+M.')}
+        author_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('author:') + '(' + urllib.quote('"Huchra, John" AND "Macri, Lucas M."') + ')',
+                         author_search) # authors, semicolon separator
+
+        req.args = {'author': urllib.quote('Huchra,+John'), 'aut_xct': 'YES'}
+        author_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('=author:') + '(' + urllib.quote('"Huchra, John"') + ')', 
+                         author_search) # author with exact match
+
+        req.args = {'author': urllib.quote('Huchra,+John;Macri,+Lucas+M.'), 'aut_logic':'OR'}
+        author_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('author:') + '(' + urllib.quote('"Huchra, John" OR "Macri, Lucas M."') + ')', 
+                         author_search) # authors with or
+
+
+
+    def test_classic_parameters_object(self):
+        # create prepared request to send to convert url code
+        req = Request('get', 'http://test.test?') 
+        prepped = req.prepare()
+        req.args = {}
+        req.mimetype = None
+        req.args = {'object': urllib.quote('M31')}
+        object_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('object:') + '(' + urllib.quote('M31') + ')', object_search) # single object
+
+        req.args = {'object': urllib.quote('M31\r\nM32')}
+        object_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('object:') + '(' + urllib.quote('M31 AND M32') + ')', 
+                         object_search) # objects, newline separator
+
+        req.args = {'object': urllib.quote('M31;M32;M33')}
+        object_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('object:') + '(' + urllib.quote('M31 AND M32 AND M33') + ')', 
+                         object_search) # object, semicolor separator
+
+    def test_classic_parameters_title(self):
+        req = Request('get', 'http://test.test?') 
+        prepped = req.prepare()
+        req.args = {}
+        req.mimetype = None
+        req.args = {'title': urllib.quote('M31')}
+        object_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('title:') + '(' + urllib.quote('M31') + ')', object_search) # single object
+        
+    def test_classic_parameters_text(self):
+        req = Request('get', 'http://test.test?') 
+        prepped = req.prepare()
+        req.args = {}
+        req.mimetype = None
+        req.args = {'text': urllib.quote('M31')}
+        object_search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('abs:') + '(' + urllib.quote('M31') + ')', object_search) # single object
+        
+
+    def test_classic_parameters_date(self):
+        req = Request('get', 'http://test.test?') 
+        prepped = req.prepare()
+        req.args = {}
+        req.mimetype = None
+        req.args = {}
+        search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=*:*', search)  # no pub date
+
+        req.args = {'start_year': 1990, 'end_year': 1991}
+        search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('pubdate:[1990-01 TO 1991-12]'), search)  # both years only
+
+        req.args = {'start_year': 1990, 'start_mon': 5, 'end_year': 1991, 'end_mon': 10}
+        search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('pubdate:[1990-05 TO 1991-10]'), search) # years and months
+ 
+        req.args = {'start_year': 1990, 'end_year': 1991, 'end_mon': 10}
+        search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('pubdate:[1990-01 TO 1991-10]'), search) # no start mon
+       
+        req.args = {'start_year': 1990, 'start_mon': 5, 'end_year': 1991}
+        search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('pubdate:[1990-05 TO 1991-12]'), search) # no end mon
+
+        req.args = {'start_year': 1990, 'start_mon': 5}
+        search = ClassicSearchRedirectView.convert_search(req)
+        n = datetime.now()
+        self.assertEqual('q=' + urllib.quote('pubdate:[1990-05 TO {:04d}-{:02d}]'.format(n.year, n.month)), search) # no end
+        req.args = {'end_year': 1991, 'end_mon': 10}
+        search = ClassicSearchRedirectView.convert_search(req)
+        self.assertEqual('q=' + urllib.quote('pubdate:[0000-01 TO 1991-10]'), search) # no start
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
--- tugboat/views.py
+++ tugboat/views.py
@@ -10,6 +10,7 @@ from flask.ext.restful import Resource
 from webargs import fields
 from webargs.flaskparser import parser
 import urllib
+from datetime import datetime
 
 class IndexView(Resource):
     """
@@ -27,47 +28,193 @@ class IndexView(Resource):
 class ClassicSearchRedirectView(Resource):
     """
     End point converts classic search to bumbblebee, returns an http redirect
+
+    Where possible, returned query should be human readable and illustrate what a good query looks like
     """
     def get(self):
         """
 
         """
-        # Setup the data
         current_app.logger.info('Classic search redirect received data, headers: {}'.format(request.headers))
-        data = get_post_data(request)
+        # data = get_post_data(request)
         bbb_url=current_app.config['BUMBLEBEE_URL']
-        return redirect(bbb_url, code=302)        
+        bbb_query = self.convert_search(request)
+        return redirect(bbb_url + bbb_query, code=302)        
 
     @staticmethod
     def convert_search(request):
         """
         Convert classic search parameters to bumblebee
         """
+
+        # schema for params
         api = {
-            'author': fields.Str(required=False)
+            'aut_xct': fields.Str(required=False),
+            'aut_logic': fields.Str(required=False),
+            'author': fields.Str(required=False),
+            'end_mon': fields.Integer(required=False),
+            'end_year': fields.Integer(required=False),
+            'obj_logic': fields.Str(required=False),
+            'object': fields.Str(required=False),
+            'start_mon': fields.Integer(required=False),
+            'start_year': fields.Integer(required=False),
+            'text': fields.Str(required=False),
+            'txt_logic': fields.Str(required=False),
+            'title': fields.Str(required=False),
+            'ttl_logic': fields.Str(required=False),
             }
         args = parser.parse(api, request)
+
+        connector = ' '
         search_terms = []
         filter_terms = []
+        search = ClassicSearchRedirectView.convert_authors(args)
+        search += ClassicSearchRedirectView.convert_typical(args, 'object', 'object')
+        search += ClassicSearchRedirectView.convert_typical(args, 'title', 'title')
+        search += ClassicSearchRedirectView.convert_typical(args, 'text', 'abs')
+        search += ClassicSearchRedirectView.convert_pubdate(args)
         for key in args:
-            if key == 'author':
-                value = urllib.unquote(args['author'])
-                value = value.replace('+', ' ')
-                search_terms.append('author:"' + value + '"')
+            # handle remaining query terms
+            pass
 
-        if len(search_terms) == 0:
+        if len(search) == 0:
             search = '*:*'
-        elif len(search_terms) == 1:
-            search = search_terms[0]
-        else:
-            search = ''
-            connector = '+OR+'
-            for current in search_terms:
-                search = current + connector
-            search = search[:-len(connector)]  # remove final
 
         solr_query = 'q=' + search
         return solr_query
+
+    @staticmethod 
+    def author_exact(args):
+        """given an aut_xct value, is it true"""
+        value = args.get('aut_xct')
+        if value:
+            if 'YES' == str(value).upper():
+                return True
+        return False
+
+    @staticmethod
+    def get_logic(classic_param, args):
+        """given a logic parameter, return its value canonical form"""
+        classic_param_to_logic = {'author': 'aut_logic', 'title': 'ttl_logic',
+                                  'text': 'txt_logic', 'object': 'obj_logic'}
+        if classic_param in args:
+            logic_param = classic_param_to_logic[classic_param]
+            value = args.get(logic_param)
+            if value:
+                value = value.upper()
+            return value
+        return ''
+
+
+    @staticmethod
+    def convert_authors(args):
+        """return string with all author search elements
+
+        classic ui allows different and/or between authors and objects so we handle 
+        just the author affecting elements here
+        """
+
+        connector = ' AND '
+        search = ''
+        logic = ClassicSearchRedirectView.get_logic('author', args)
+        exact = ClassicSearchRedirectView.author_exact(args)
+        if logic == 'OR':
+            connector = ' OR '
+        author_field = 'author:'
+        if exact:
+            author_field = '=author:'
+        # one lone parameter should hold all authors from classic
+        authors_str = args.get('author')
+        if authors_str:
+            authors = ClassicSearchRedirectView.classic_field_to_array(authors_str)
+            search += urllib.quote(author_field) + '('
+            for author in authors:
+                search += urllib.quote(author + connector)
+            search = search[:-len(urllib.quote(connector))]  # remove final
+            search += ')'
+        return search
+
+
+
+    @staticmethod
+    def convert_typical(args, classic_param, bbb_param):
+        """return string with all search elements
+
+        returned string should strive for human readability and use prefered query format
+        """
+        connector = ' AND '
+        search = ''
+        logic = ClassicSearchRedirectView.get_logic(classic_param, args)
+        if logic == 'OR':
+            connector = ' OR '
+        # one lone parameter should hold all authors from classic
+        classic_str = args.get(classic_param)
+        if classic_str:
+            terms = ClassicSearchRedirectView.classic_field_to_array(classic_str)
+            search += urllib.quote(bbb_param + ':') + '('
+            for term in terms:
+                search += urllib.quote(term + connector)
+            search = search[:-len(urllib.quote(connector))]  # remove final connector
+            search += ')'
+        return search
+
+
+    @staticmethod
+    def convert_pubdate(args):
+        """return string with pubdate element
+
+        for pubdate date search, only start or end need be specified
+        at least one start year or end year must be provided
+        unlike entrdy date search, negitive offsets are not supported
+        in classic, 2 digit years were permitted
+        bumblebee example: pubdate:[1990-01 TO 1990-02]
+        """
+        start_year = args.get('start_year')
+        end_year = args.get('end_year')
+        if start_year is None and end_year is None:
+            return ''
+        
+        start_month = args.get('start_mon')
+        end_month = args.get('end_mon')
+
+        if start_year is None:
+            start_year = 0
+        if start_month is None:
+            start_month = 1
+
+        tmp = datetime.now()
+        if end_year is None:
+            end_year = tmp.year
+            if end_month is None:
+                end_month = tmp.month
+        else:
+            if end_month is None:
+                end_month = 12
+
+        # Y10k problem, but for 2 digit years we want to be clear what years we are searching
+        search = 'pubdate' + urllib.quote(':[{:04d}-{:02d} TO {:04d}-{:02}]'.format(start_year, start_month,
+                                                                                    end_year, end_month))
+        return search
+        
+        
+            
+        
+    
+
+    @staticmethod
+    def classic_field_to_array(value):
+        """ convert authors or objects from classic to list"""
+        value = urllib.unquote(value)
+        value = value.replace('\r\n', ';')
+        values = value.split(';')
+        for i in range(0, len(values)):
+            values[i] = values[i].replace('+', ' ')
+            if ' ' in values[i] and (values[i].startswith('"') and values[i].endswith('"')) is False:
+                # value has space and is not already surrounded by double quotes so we add quotes
+                values[i] = '"' + values[i] + '"'
+        return values
+
+
     
     
 class BumblebeeView(Resource):
