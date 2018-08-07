11d86c8242805654e9a71a9092a39db7880bb01f
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2018-08-07 09:59:11 -0400

    Enabled JSON stdout logging and HTTP connection pool (#57)

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 
+LOG_STDOUT = True
 VAULT_QUERY_URL = 'https://api.adsabs.harvard.edu/v1/vault/query'
 BUMBLEBEE_URL = 'https://qa.adsabs.harvard.edu'
 API_DEV_KEY= 'token to be added when deployed'
